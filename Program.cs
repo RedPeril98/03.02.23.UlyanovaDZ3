8959150297562a83c6949b5a6ceef46e7cf71573
Дарья <red_peril@bk.ru>
2023-02-03 11:53:33 +0300

    задача 23

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ else Console.WriteLine($"Введи правильное число");
 */
 
 // Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
-
+/*
 int x1 = Coordinate("x", "A");
 int y1 = Coordinate("y", "A");
 int z1 = Coordinate("z", "A");
@@ -42,3 +42,32 @@ Math.Pow((z2-z1), 2));
 double segmentLength = Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
 
 Console.WriteLine($"Длина отрезка {segmentLength}");
+*/
+
+//Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+/*
+Console.Write("Введите число: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube){
+int counter = 0;
+int length = cube.Length;
+while (counter < length){
+cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+counter++;
+}
+}
+
+void PrintArry(int[] coll){
+int count = coll.Length;
+int index = 0;
+while(index < count){
+Console.Write(coll[index]+ " ");
+index++;
+}
+}
+
+int[] arry = new int[cube+1];
+Cube(arry);
+PrintArry(arry);
+*/
